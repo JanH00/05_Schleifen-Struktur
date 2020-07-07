0ac076657511ffca1da17dc1027568650373fff8
JanH00 <67894980+JanH00@users.noreply.github.com>
2020-07-07 14:16:06 +0200

    Create 05_Schleifen-Struktur.dart

---
+++ 05_Schleifen-Struktur.dart
@@ -0,0 +1,17 @@
+/*
+void main() {
+  int limit = 5;
+  for(int i = 0; i < limit; i++) {
+    String b = i.toString();
+    print(b);
+  }
+}
+*/
+
+void main() {
+    int limit = 5;
+  for(int i = 0; i < limit; i++) {
+    String b = i.toString();
+    print("OK dann halt so $b");
+  }
+}
